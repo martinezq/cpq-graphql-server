384b2f13911d82ef950a85f2b007f96782341517
Marcin Koch <marcin.koch@tacton.com>
2021-12-28 12:37:27 +0100

    schema generator

--- src/init/schema-generator.js
+++ src/init/schema-generator.js
@@ -2,27 +2,44 @@ const axios = require('axios').default;
 const parser = require('xml2json');
 
 async function generateSchema(baseUrl, headers) {
-    if (!headers) {
-        return Promise.resolve('type Query { ping: String }');
+    const url = `${baseUrl}/api/describe`;
+
+    console.log('LOADING schema from', url);
+
+    try {
+        const resp = await axios.get(url, { headers: { Authorization: 'Basic YXBpOlRhY3RvblBvd2VyMSE=' } });
+
+        const { data } = resp;
+        const jsonData = JSON.parse(parser.toJson(data));
+        const resources = jsonData.resources.resource;
+
+        console.log(resources);
+
+        const types = resources.map(r => `
+            type ${r.name} {
+                id: ID!
+                revisionId: ID!
+                name: String                
+            }
+        `);
+
+        const queries = resources.map(r => `    ${r.name.toLowerCase()}: [${r.name}]`);
+
+        const schema = `
+            type Query {
+                status: String
+                ${queries}
+            }
+
+            ${types.join('\n')}
+        `;
+
+        return schema;
+
+    } catch (e) {
+        // console.log(e.message);
+        return Promise.reject(e.message);
     }
-    const resp = await axios.get(`${baseUrl}/api/describe`, { headers });
-
-    // const { data } = resp;
-    // const jsonData = JSON.parse(parser.toJson(data));
-
-    console.log('z');
-    
-    return Promise.resolve(`
-        type Query {
-            account: [Account]
-        }
-
-        type Account {
-            id: ID!
-            revisionId: ID!
-            name: String
-        }
-    `);
 }
 
 module.exports = {
--- src/init/server.js
+++ src/init/server.js
@@ -25,9 +25,9 @@ async function handlePath(req, res, next, app) {
     const path = req.path;
 
     if (routes[path]) {
-        console.log('FOUND apollo server for', path);
+        // console.log('FOUND apollo server for', path);
 
-        if (routes[path].state !== 'ready') {
+        if (routes[path].state === 'init' && req.headers.baseurl) {
 
             const typeDefs = 'type Query { status: String }'
             const resolvers = {
@@ -36,12 +36,23 @@ async function handlePath(req, res, next, app) {
                 }
             };
 
-            const server = await createApolloServer(typeDefs, resolvers);
-            
-            await replaceApolloServer(server, app, path);
-            console.log('RECREATED apollo server for', path);
+            routes[path].state = 'fetching';
 
-            routes[path].state = 'ready';
+            try {
+                const schema = await generateSchema(`https:/${path}`, req.headers);
+
+                const typeDefsMerged = typeDefs + '\n' + schema;
+
+                const server = await createApolloServer(path, typeDefsMerged, resolvers);
+            
+                await replaceApolloServer(server, app, path);
+                console.log('RECREATED apollo server for', path);
+
+                routes[path].state = 'ready';
+            } catch (e) {
+                console.log(e);
+                routes[path].state = 'error';
+            }
         };
 
         routes[path].router(req, res, next);
@@ -49,7 +60,7 @@ async function handlePath(req, res, next, app) {
     } else {
         console.log('NOT FOUND apollo server for', path, ', starting...');
 
-        const server = await createInitialApolloServer();
+        const server = await createInitialApolloServer(path);
         await registerApolloServer(server, app, path);
 
         console.log('STARTED apollo server under', path)
@@ -78,22 +89,22 @@ async function replaceApolloServer(server, app, path) {
     return registerApolloServer(server, app, path);
 }
 
-async function createInitialApolloServer() {
+async function createInitialApolloServer(path) {
     const typeDefs = 'type Query { status: String }'
     const resolvers = {
         Query: {
             status: () => 'init'
         }
     };
-    return createApolloServer(typeDefs, resolvers);
+    return createApolloServer(path, typeDefs, resolvers);
 }
 
-async function createApolloServer(typeDefs, resolvers) {
+async function createApolloServer(path, typeDefs, resolvers) {
     const server = new ApolloServer({
         typeDefs,
         resolvers,
         context: ({ req }) => ({
-            // baseUrl: req.headers.baseurl,
+            baseUrl: `http:/${path}`,
             headers: {
                 authorization: req.headers.authorization
             }
